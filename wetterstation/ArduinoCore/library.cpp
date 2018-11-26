01e8afcc5063c7f7e932f0c5665337294c5897e4
Unknown <andres.minder@students.fhnw.ch>
2018-11-26 11:58:00 +0100

    Init Project

---
+++ wetterstation/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 22.11.2018 17:26:27
+ * Author : Andres
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
---
+++ wetterstation/wetterstation/Sketch.cpp
@@ -0,0 +1,24 @@
+﻿#include <Arduino.h>
+
+#include <Arduino.h>
+#include <SD.h>
+#include <SPI.h>
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+const int chipSelect = 53;
+
+File myFile;
+
+void setup()
+{
+	Serial.begin(9600);
+
+}
+
+void loop()
+{
+
+}
